32f6576539ad741dae9b4649bdc201b5be7ea03e
reza173040113 <reza_173040113@mail.unpas.ac.id>
2020-12-04 16:06:36 +0700

    Tambah Kelas TIM

---
+++ src/main/java/org/ifunpas/kpl/kode/Tim.java
@@ -0,0 +1,16 @@
+package org.ifunpas.kpl.kode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tim {
+	private String nama;
+//	private List<Pemain> pemainInti = new ArrayList<Pemain>();
+	
+	public Tim() {
+		nama = "";
+	}
+	public Tim(String nama) {
+		this.nama = nama;
+	}
+}
