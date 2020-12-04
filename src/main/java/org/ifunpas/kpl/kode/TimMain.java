529b97a816e1e290c79d742aa633284a3689c9af
nurulfauziahh <nurul_173040092@mail.unpas.ac.id>
2020-12-04 16:26:15 +0700

    Buat Daftar pemain

--- src/main/java/org/ifunpas/kpl/kode/TimMain.java
+++ src/main/java/org/ifunpas/kpl/kode/TimMain.java
@@ -2,7 +2,19 @@ package org.ifunpas.kpl.kode;
 
 public class TimMain {
 	
-
-	
+	public static void main(String[] args) {
+		Tim tim = new Tim("Persib");
+		
+		Pemain pemain1 = new Pemain("I Made", 1);
+		Pemain pemain2 = new Pemain("Atep", 7);
+		Pemain pemain3 = new Pemain("Essien", 5);
+		
+		tim.tambahPemainInti(pemain1);
+		tim.tambahPemainInti(pemain2);
+		tim.tambahPemainInti(pemain3);
+		tim.tambahPemainInti(new Pemain("Febry", 22));
+		
+		tim.tampilPemainInti();
+	}
 
 }
